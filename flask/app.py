032ea8c25b5e6356106d72587a701f2391ac670e
tanmoyhalder <tanmoyhalder123@gmail.com>
2022-08-21 17:09:06 +0530

    Add new route to update frontend from backend

--- flask/app.py
+++ flask/app.py
@@ -5,17 +5,20 @@ app = Flask(__name__)
 
 # Route
 @app.route('/')
-
 def index():
-
     return "Hello Tanmoy. Great job for creating a new flask app"
 
 # Adding HTMl
 
 @app.route('/home')
-
 def home():
     return render_template('home.html')
+
+@app.route('/about')
+def about():
+
+    objective = 'Objective is to build a small and basic working flask app.'
+    return render_template('about.html', objective = objective)
 
 # Templating
 
